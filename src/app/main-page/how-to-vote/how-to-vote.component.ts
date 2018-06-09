d18e3ac78fe9254dc4e7eef43ec7388c0e986a83
Maleta <nmmaleta@gmail.com>
2018-06-09 18:47:14 +0200

    Adding all components

---
+++ src/app/main-page/how-to-vote/how-to-vote.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-how-to-vote',
+  templateUrl: './how-to-vote.component.html',
+  styleUrls: ['./how-to-vote.component.css']
+})
+export class HowToVoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
